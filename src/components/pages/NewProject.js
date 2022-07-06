606ffb5484938d2409fa9981b0b4d939ed9d34c1
Yago Motti <yago.motti@easyworkoutsourcing.com>
2022-07-06 15:15:30 -0300

    Messages and new project

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,40 @@
+import {useNavigate} from 'react-router-dom'
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject(){
+
+    const history = useNavigate()
+
+    function createPost(project){
+        //initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+        .then(resp => resp.json())
+        .then((data) => {
+            console.log(data)
+            //redirect
+            history('/projects', {state:{message: 'Project created successfully!'}})
+        })
+        .catch(err => console.log(err))
+    }
+
+
+
+
     return (
         <div className={styles.newproject_container}>
             <h1>Create Project</h1>
             <p>Create a project to add your servicing</p>
-            <ProjectForm btnText="Create Project"/>
+            <ProjectForm handleSubmit={createPost} btnText="Create Project"/>
         </div>
     )
     }
